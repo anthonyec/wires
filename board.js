1454374401b81692d31ffb9b4aa05e29ab3effd9
Anthony Cossins <anthonyecossins@gmail.com>
2019-07-04 22:18:16 +0100

    Trying path tracing

--- board.js
+++ board.js
@@ -1,3 +1,7 @@
+function Log({ value = '' } = {}) {
+  console.log('Log!', value);
+}
+
 function Toggle({ toggledOn = true } = {}) {
   return { out: toggledOn }
 }
@@ -23,7 +27,6 @@ function Board() {
     connections: [],
 
     executeComponent: function(uid, component, props = {}) {
-
       const storedComponent = this.components[uid];
       const componentComputedProps = {
         ...storedComponent.props,
@@ -34,14 +37,47 @@ function Board() {
 
       const connections = this.connections.filter((connection) => {
         return connection.from.uid === uid;
-      }).map((connection) => {
-        return connection.to;
       });
 
       console.log(connections);
 
-      // console.log(uid, component, props, storedComponent);
-      // console.log('execute', uid, props);
+      if (!connections.length) {
+        return;
+      }
+
+      const propsForAffectedComponent = connections.reduce((mem, connection) => {
+        if (!mem[connection.to.prop]) {
+          mem[connection.to.prop] = componentComputedProps[connection.from.prop]
+        }
+
+        return mem;
+      }, {});
+
+      const affectedComponent = connections.reduce((mem, connection) => {
+        const storedAffectedComponent = this.components[connection.to.uid];
+        const newProps = {
+          ...storedAffectedComponent.props,
+          ...propsForAffectedComponent
+        };
+
+        if (!mem[connection.to.uid]) {
+          return {
+            ...storedAffectedComponent,
+            ...{ props: newProps }
+          }
+        }
+
+      }, {});
+
+      this.components[affectedComponent.uid] = affectedComponent;
+
+      this.components[affectedComponent.uid].execute(affectedComponent.uid);
+
+      // this.components[affectedComponent.uid].execute(
+      //   this.components[affectedComponent.uid].uid,
+      //   this.components[affectedComponent.uid],
+      //   this.components[affectedComponent.uid].props
+      // );
     },
 
     getUid(name) {
@@ -61,6 +97,7 @@ function Board() {
       const createdComponent = {
         uid,
         props: {},
+        executor: component,
         execute: (props) => this.executeComponent.call(
           this,
           uid,
@@ -105,6 +142,7 @@ const board = new Board();
 const toggle = board.createComponent(Toggle);
 const toggle2 = board.createComponent(Toggle);
 const andGate = board.createComponent(AndGate);
+const log = board.createComponent(Log);
 
 console.log('board.components', board.components);
 
@@ -112,6 +150,7 @@ console.log('--- connections ---');
 
 board.connect(toggle, 'out').to(andGate, 'in1');
 board.connect(toggle2, 'out').to(andGate, 'in2');
+board.connect(andGate, 'out').to(log, 'value');
 
 console.log(' ');
 console.log('---- simulate ----');
---
+++ board_trace.js
@@ -0,0 +1,166 @@
+function Node({ in1 } = {}) {
+  return { out: in1 };
+}
+
+function Log({ value = '' } = {}) {
+  console.log('Log!', value);
+}
+
+function Toggle({ toggledOn = true } = {}) {
+  return { out: toggledOn }
+}
+
+function AndGate({ in1 = false, in2 = false } = {}) {
+  return {
+    out: in1 && in2
+  };
+};
+
+function OrGate({ in1, in2 } = {}) {
+  return {
+    out: in1 || in2
+  };
+};
+
+
+function Board() {
+  const board = {
+    usedNames: {},
+
+    components: {},
+    connections: [],
+
+    tracePath: function({ to: { uid }}, path = [], callback = () => {}) {
+      const nextConnection = this.connections.filter((connection) => {
+        return connection.from.uid === uid;
+      });
+
+      if (nextConnection.length) {
+        const mem = [...path, nextConnection[0]];
+        this.tracePath.call(this, nextConnection[0], mem, callback);
+      } else {
+        callback(path);
+      }
+    },
+
+    executeComponent: function(uid) {
+      console.log('executeComponent:', uid);
+
+      const connections = this.connections.filter((connection) => {
+        return connection.from.uid === uid;
+      });
+
+      connections.forEach((connection) => {
+        console.log(' ');
+        console.log('PATH ---');
+        this.tracePath(connection, [], (path) => {
+          const newPath = [...[connection], ...path];
+          const pathString = newPath.reduce((mem, step, index) => {
+            return mem + this.getConnectionAsString(step, index !== 0);
+          }, '');
+
+          console.log(pathString);
+        });
+      });
+
+      // connections.forEach(connection => console.log(this.getConnectionAsString(connection)));
+      console.log(' ')
+    },
+
+    createComponent: function (component) {
+      const { name } = component;
+      const uid = this.getUid(name);
+      const createdComponent = {
+        uid,
+        props: {},
+        // executor: component,
+        execute: (props) => this.executeComponent.call(
+          this,
+          uid
+          // component,
+          // props
+        )
+      };
+
+      const newComponents = {
+        ...this.components,
+        ...{ [uid]: createdComponent }
+      };
+
+      this.components = newComponents;
+      this.lastId += 1;
+
+      return createdComponent;
+    },
+
+    getConnectionAsString(connection, ignoreStart) {
+      const startUid = !ignoreStart ? `(${connection.from.uid})` : '';
+      return `${startUid}[${connection.from.prop}] ---> [${connection.to.prop}](${connection.to.uid})`;
+    },
+
+    getUid(name) {
+      if (!this.usedNames[name]) {
+        this.usedNames[name] = 1;
+      }
+
+      const uid = `${name}_${this.usedNames[name]}`;
+      this.usedNames[name] += 1;
+
+      return uid;
+    },
+
+    connect: function (createdComponent, outlet) {
+      const { uid: fromUid } = createdComponent;
+
+      return {
+        to: (createdComponent, inlet) => {
+          const { uid: toUid } = createdComponent;
+          const newConnection = {
+            from: { uid: fromUid, prop: outlet },
+            to: { uid: toUid, prop: inlet },
+          };
+          const newConnections = [
+            ...this.connections,
+            newConnection
+          ];
+
+          this.connections = newConnections;
+
+          console.log(`connect: ${fromUid} [${outlet}] ---> [${inlet}] ${toUid}`);
+        }
+      };
+    }
+  };
+
+  return board;
+}
+
+const board = new Board();
+
+const toggle = board.createComponent(Toggle);
+const toggle2 = board.createComponent(Toggle);
+const andGate = board.createComponent(AndGate);
+const log = board.createComponent(Log);
+const node = board.createComponent(Node);
+const node2 = board.createComponent(Node);
+const node3 = board.createComponent(Node);
+
+console.log(board.components);
+
+console.log(' ');
+console.log('--- connections ---');
+
+board.connect(toggle, 'out').to(andGate, 'in1');
+board.connect(toggle2, 'out').to(andGate, 'in2');
+board.connect(andGate, 'out').to(log, 'value');
+
+board.connect(toggle, 'out').to(node, 'in1');
+board.connect(node, 'out').to(node2, 'in1');
+board.connect(node2, 'out').to(node3, 'in1');
+board.connect(node3, 'out').to(log, 'value');
+
+console.log(' ');
+console.log('---- simulate ----');
+
+toggle.execute();
+toggle2.execute({ toggledOn: true });
