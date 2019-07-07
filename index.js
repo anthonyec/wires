41ce3bf48b602a222d602190bc509677d1d2f691
Anthony Cossins <anthonyecossins@gmail.com>
2019-07-07 10:43:04 +0100

    Plan out how to do the execution

--- index.js
+++ index.js
@@ -7,33 +7,27 @@ const graph = createGraph();
 
 const andGate = graph.createComponent(AndGate);
 const log = graph.createComponent(Log);
+const log2 = graph.createComponent(Log);
 const toggle1 = graph.createComponent(Toggle);
 const toggle2 = graph.createComponent(Toggle);
 
+/*
+                  .-> [value]log2
+  toggle1[out] ---
+                  `-> [in1]
+                          andGate[out] ---> [value]log
+                  .-> [in2]
+  toggle2[out] ---
+*/
+
+console.log('-- connections --');
+graph.from(toggle1).link('out', 'value').to(log2);
 graph.from(toggle1).link('out', 'in1').to(andGate);
 graph.from(toggle2).link('out', 'in2').to(andGate);
 graph.from(andGate).link('out', 'value').to(log);
+console.log('');
 
+console.log('-- execution --');
 toggle1.execute({ toggledOn: true });
-toggle2.execute({ toggledOn: true });
-
-// console.log(graph);
-
-
-function mapOutletPropsToInletProps(outletProps = {}, inletProps = {}, mapping = {}) {
-  const mapped = Object.keys(inletProps).reduce((mem, inletProp) => {
-    const outletMapping = mapping[inletProp];
-
-    // Exists in mapping
-    if (outletMapping) {
-      mem[inletProp] = outletProps[outletMapping];
-    }
-
-    return mem;
-  }, inletProps);
-
-  return mapped;
-}
-
-const t = mapOutletPropsToInletProps({ toggledOn: true }, { in1: false, in2: false }, { in1: 'toggledOn' });
-console.log(t);
+// toggle2.execute({ toggledOn: true });
+console.log('');
