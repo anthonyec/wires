c831970979e44b754dee6c70125c9a26fbc64474
Anthony Cossins <anthonyecossins@gmail.com>
2019-07-07 11:17:36 +0100

    Add anom node to show piping

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const Toggle = require('./components/toggle');
 
 const graph = createGraph();
 
+const node = graph.createComponent(({ in1 = false } = {}) => {
+  return { out: in1 + 'wow' }
+});
 const andGate = graph.createComponent(AndGate);
 const log = graph.createComponent(Log);
 const log2 = graph.createComponent(Log);
@@ -12,7 +15,7 @@ const toggle1 = graph.createComponent(Toggle);
 const toggle2 = graph.createComponent(Toggle);
 
 /*
-                  .-> [value]log2
+                  .-> [in1]node[out] ---> [value]log2
   toggle1[out] ---
                   `-> [in1]
                           andGate[out] ---> [value]log
@@ -21,7 +24,8 @@ const toggle2 = graph.createComponent(Toggle);
 */
 
 console.log('-- connections --');
-graph.from(toggle1).link('out', 'value').to(log2);
+graph.from(toggle1).link('out', 'in1').to(node);
+graph.from(node).link('out', 'value').to(log2);
 graph.from(toggle1).link('out', 'in1').to(andGate);
 graph.from(toggle2).link('out', 'in2').to(andGate);
 graph.from(andGate).link('out', 'value').to(log);
