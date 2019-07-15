7fcc122d520e4decde6b7279d664a3e041c005c5
Anthony Cossins <anthonyecossins@gmail.com>
2019-07-15 10:17:13 +0100

    Export types

--- packages/wires-components/src/index.js
+++ packages/wires-components/src/index.js
@@ -1,3 +1,11 @@
+import * as types from './types';
+
 export * from './debug';
 export * from './json';
 export * from './logic';
+
+const WiresComponents = {
+  types
+};
+
+export default WiresComponents;
--- packages/wires-components/src/types.js
+++ packages/wires-components/src/types.js
@@ -3,3 +3,5 @@ export const STRING = Symbol('string');
 export const NUMBER = Symbol('number');
 export const OBJECT = Symbol('object');
 export const ARRAY = Symbol('array');
+export const VECTOR2D = Symbol('vector2d');
+export const VECTOR3D = Symbol('vector3d');
